c6de0c512d8b36f782bd6800fa977b1d3c753f4a
Brandon Li <brandon.li.gn@gmail.com>
2026-07-30 00:46:37 -0400

    Allow configured Drive scope for temporary render upload

--- scripts/upload-drive.sh
+++ scripts/upload-drive.sh
@@ -47,9 +47,9 @@ RCLONE_SCOPE="$(awk '
 ' "$RCLONE_CONFIG_FILE")"
 
 case "$RCLONE_SCOPE" in
-  drive.file|https://www.googleapis.com/auth/drive.file) ;;
+  drive.file|https://www.googleapis.com/auth/drive.file|drive|https://www.googleapis.com/auth/drive) ;;
   *)
-    echo "The gdrive remote must use the least-privilege drive.file scope." >&2
+    echo "The gdrive remote must use an approved Drive scope." >&2
     exit 65
     ;;
 esac
